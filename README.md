96f30c76d0bd3de9f2f51b4b31957b0cfa6aa1f9
Dawn DeWaay <99098265+DawnDeWaay@users.noreply.github.com>
2025-12-05 13:03:16 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# 090 Create Task
+
+## Agile vs Waterfall
+[![Agile vs Waterfall](https://img.youtube.com/vi/EpV8d7Tq4qU/0.jpg)](https://www.youtube.com/watch?v=EpV8d7Tq4qU)
+
+``` 
+When a new task is added with the status todo
+Then I will see the new task in the todo column 
+```
+
+``` 
+When a new task is added with the status doing
+Then I will see the new task in the doing column 
+```
+
+``` 
+When a new task is added with the status done
+Then I will see the new task in the done column 
+```
+
+the task board
+- should create a new task when a new description and status is entered and display the task in the status column that corresponds to the task status
+
+Building toward CSTA Standards:
+- Develop programs for multiple computing platforms. (3B-AP-19) https://www.csteachers.org/page/standards
+- Construct solutions to problems using student-created components, such as procedures, modules and/or objects. (3B-AP-14) https://www.csteachers.org/page/standards
+- Use lists to simplify solutions, generalizing computational problems instead of repeatedly using simple variables. (3A-AP-14) https://www.csteachers.org/page/standards
+
+Copyright &copy; 2023 Knight Moves. All Rights Reserved.
---
+++ test/verify.test.js
@@ -0,0 +1,60 @@
+const http = require("http");
+const fs = require("fs");
+const puppeteer = require("puppeteer");
+const { assert } = require("console");
+
+let server;
+let browser;
+let page;
+
+beforeAll(async () => {
+  server = http.createServer(function (req, res) {
+    fs.readFile(__dirname + "/.." + req.url, function (err, data) {
+      if (err) {
+        res.writeHead(404);
+        res.end(JSON.stringify(err));
+        return;
+      }
+      res.writeHead(200);
+      res.end(data);
+    });
+  });
+
+  server.listen(process.env.PORT || 3000);
+});
+
+afterAll(() => {
+  server.close();
+});
+
+beforeEach(async () => {
+  browser = await puppeteer.launch();
+  page = await browser.newPage();
+  await page.goto("http://localhost:3000/index.html");
+});
+
+afterEach(async () => {
+  await browser.close();
+});
+
+describe('the index.js file', () => {
+  it('should create a new task when a new description and status is entered and display the task in the status column that corresponds to the task status', async function() {
+    const todoCardsBefore = await page.$$eval('#todo-cards > .card', (results) => results );
+    expect(todoCardsBefore.length).toBe(3);
+      
+    await page.evaluate(() => {
+      document.getElementById('task-description').value = 'test description';
+      document.getElementById('task-status').value = 'todo';
+      document.getElementById('add-task').click();
+    });
+    
+    const todoCardsAfter = await page.$$eval('#todo-cards > .card', (results) => results );
+    expect(todoCardsAfter.length).toBe(4);
+    
+    const card = await page.$eval('#task-8', (result) => result.innerHTML );
+    
+    expect(card).toContain('test description');
+    expect(card).toMatch(/<a href="\/edit\/8">[\s\S]*Edit[\s\S]*<\/a>/);
+    expect(card).toMatch(/<a href="\/delete\/8">[\s\S]*Delete[\s\S]*<\/a>/);
+  });
+});
